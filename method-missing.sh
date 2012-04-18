a0dbdc4709cf04bcf1120502429c770ca856aafb
Erich Smith <heartquest@gmail.com>
2012-04-18 14:04:47 -0400

    refactor for dual-use

--- method-missing.sh
+++ method-missing.sh
@@ -1,18 +1,48 @@
-# Method missing for bash. Add to your .bashrc
+#!/bin/bash
 
-method_missing() { local cmd=$1
+# Extensible #method_missing for bash
+load_source() {
+  method_missing() { local cmd=$1
     shopt -u extdebug
     if ! $(type -t $cmd >/dev/null); then
-        newcmd=$(~/conf/methodfoundry.rb $cmd)
-        if [ -n "$newcmd" ]; then
-            echo "$FUNCNAME: executing '$newcmd' instead"
-            eval $newcmd
-            shopt -s extdebug
-            return 2
-        else
-            echo "$FUNCNAME: no match found for $cmd"
-        fi
+      newcmd=$(~/conf/methodfoundry.rb $cmd)
+      if [ -n "$newcmd" ]; then
+        echo "$FUNCNAME: executing '$newcmd' instead"
+        eval $newcmd
+        shopt -s extdebug
+        return 2
+      else
+        echo "$FUNCNAME: no match found for $cmd"
+      fi
     fi
+  }
+
+  trap 'method_missing $BASH_COMMAND' DEBUG
 }
 
-trap 'method_missing $BASH_COMMAND' DEBUG
+install() {
+  echo 'installing MethodFoundry...'
+
+  # find our absolute PATH
+  SOURCE="${BASH_SOURCE[0]}"
+  while [ -h "$SOURCE" ] ; do SOURCE="$(readlink "$SOURCE")"; done
+  DIR="$( cd -P "$( dirname "$SOURCE" )" && pwd )"
+
+  # source this file in .bashrc
+  done_previously() { [ ! -z "$(grep source | grep $DIR | grep $(basename $0))" ]; }
+
+  if ! $(<~/.bashrc done_previously) && ! $(<~/.bash_profile done_previously); then
+    echo 'sourcing from .bashrc...'
+    echo "source $DIR/$(basename $0)" >> ~/.bashrc
+  fi
+
+  echo 'done'
+}
+
+if [[ "$BASH_SOURCE" == "$0" ]]; then
+  install
+else
+  load_source
+  unset install
+  unset load_source
+fi
